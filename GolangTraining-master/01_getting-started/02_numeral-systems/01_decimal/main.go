841a7d9015dd7bf2c9f538edb38887c0c08a832f
Sean Fitzgerald <seanfitz97@gmail.com>
2024-01-18 20:43:12 -0500

    update

--- GolangTraining-master/01_getting-started/02_numeral-systems/01_decimal/main.go
+++ GolangTraining-master/01_getting-started/02_numeral-systems/01_decimal/main.go
@@ -5,4 +5,15 @@ import "fmt"
 func main() {
 	//print the number 24
 	fmt.Println(24)
+
+	//print the number 12
+	fmt.Println(12)
+
+	//make an api request to get the current price of bitcoin
+	//https://api.coindesk.com/v1/bpi/currentprice.json
+	//print the price in usd with 2 decimal places
+	//example response: 27,798.76
+	//here is that request
+	//https://api.coindesk.com/v1/bpi/currentprice/usd.json
+
 }
